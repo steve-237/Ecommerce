b7ce66b560d2675fe68d693174308d46ce1c11bf
steve-237 <steve0peg@gmail.com>
2025-02-06 14:42:30 +0100

    change selector prefix app to ecom

--- apps/ecom-frontend/src/app/app.component.ts
+++ apps/ecom-frontend/src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { FaConfig, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 
 @Component({
-  imports: [RouterModule],
-  selector: 'app-root',
+  imports: [RouterModule, FaIconComponent],
+  selector: 'ecom-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'ecom-frontend';
+export class AppComponent implements OnInit {
+  
+  private faIconLibrary = inject(FaIconLibrary);
+  private faConfig = inject(FaConfig)
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() {
+    this.faConfig.defaultPrefix = 'far';
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ apps/ecom-frontend/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,6 @@
+import { IconDefinition } from "@fortawesome/free-brands-svg-icons";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+
+export const fontAwesomeIcons: IconDefinition[] = [
+    faUser
+]
