f42f96072582dddbca9bae196e24fb83a6f21f22
steve-237 <steve0peg@gmail.com>
2025-02-06 15:45:58 +0100

    fix issue with prefix

--- apps/ecom-frontend/eslint.config.mjs
+++ apps/ecom-frontend/eslint.config.mjs
@@ -12,7 +12,7 @@ export default [
         'error',
         {
           type: 'attribute',
-          prefix: 'app',
+          prefix: ['ecom', 'app'],
           style: 'camelCase',
         },
       ],
@@ -20,7 +20,7 @@ export default [
         'error',
         {
           type: 'element',
-          prefix: 'app',
+          prefix: ['ecom', 'app'],
           style: 'kebab-case',
         },
       ],
