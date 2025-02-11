efbd4a97f05b8b9afaf1ce50cf203c5615fda559
steve-237 <steve0peg@gmail.com>
2025-02-11 18:21:27 +0100

    add backend project

---
+++ apps/ecom-backend/src/test/java/de/sweetpeakdesign/com/EcomBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.sweetpeakdesign.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
