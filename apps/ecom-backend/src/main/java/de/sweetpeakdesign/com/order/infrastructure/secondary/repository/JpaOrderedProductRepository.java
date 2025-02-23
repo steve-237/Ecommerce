d7642d0c6720b19ee4fbdec8a61196be14bd4d62
steve-237 <steve0peg@gmail.com>
2025-02-23 21:35:33 +0100

    fix bug

--- apps/ecom-backend/src/main/java/de/sweetpeakdesign/com/order/infrastructure/secondary/repository/JpaOrderedProductRepository.java
+++
@@ -1,9 +0,0 @@
-package de.sweetpeakdesign.com.order.infrastructure.secondary.repository;
-
-import de.sweetpeakdesign.com.order.infrastructure.secondary.entity.OrderedProductEntity;
-import de.sweetpeakdesign.com.order.infrastructure.secondary.entity.OrderedProductEntityPk;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JpaOrderedProductRepository extends JpaRepository<OrderedProductEntity, OrderedProductEntityPk> {
-
-}
