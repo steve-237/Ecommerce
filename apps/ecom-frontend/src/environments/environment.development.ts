bdfcd8156257a6a5fef9c5d147548eea13256096
steve-237 <steve0peg@gmail.com>
2025-02-25 23:40:12 +0100

    config environment variable

--- apps/ecom-frontend/src/environments/environment.development.ts
+++ apps/ecom-frontend/src/environments/environment.development.ts
@@ -1,5 +1,10 @@
 export const environment = {
     kinde: {
-        
-    }
-}
+        authority: "https://sweetpeakdesign.kinde.com",
+        redirectUrl: "http//localhost:4200",
+        postLogoutRedirectUri: "http://localhost:4200",
+        clientId: "12fe50507a3b48269de0686052e343a0",
+        audience: "https://sweetpeakdesign.kinde.com/api",
+    },
+    apiUrl: "http://localhost:8080/api",
+};
