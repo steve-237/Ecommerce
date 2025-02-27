b2a1aed2792a9106a7a390389acbb8e052144cd7
steve-237 <steve0peg@gmail.com>
2025-02-27 20:05:29 +0100

    update app config and add new dependencies

--- apps/ecom-frontend/src/app/app.config.server.ts
+++ apps/ecom-frontend/src/app/app.config.server.ts
@@ -1,10 +1,11 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { UNIVERSAL_PROVIDERS } from '@ng-web-apis/universal';
 
 const serverConfig: ApplicationConfig = {
   providers: [provideServerRendering(),
-   //  UNIVERSAL_PROVIDERS
+     UNIVERSAL_PROVIDERS
   ],
 };
 
