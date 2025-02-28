ea39595a44c2678c46db4cf04837fcb12100f443
steve-237 <steve0peg@gmail.com>
2025-02-28 02:18:46 +0100

    update dependency to fix bug

--- apps/ecom-frontend/src/environments/environment.ts
+++ apps/ecom-frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@ export const environment = {
         redirectUrl: "http//localhost:4200",
         postLogoutRedirectUri: "http://localhost:4200",
         clientId: "12fe50507a3b48269de0686052e343a0",
-        audience: "https://sweetpeakdesign.kinde.com/api",
+        audience: "http://localhost:8080/api",
     },
     apiUrl: "http://localhost:8080/api",
 };
